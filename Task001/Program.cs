bca393057b788935c1a973d354df10234a2c4047
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-08-25 12:33:22 +0300

    what's wrong...

---
+++ Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// разложение числа на простые числа 
+
+void SimpleSNumbers(int n)
+{
+    int m = 2;
+    while(n != 1)
+    {
+        if(n%m == 0)
+        {
+            Console.Write(m + " ");
+            SimpleSNumbers(n/m);
+        }
+        m++;
+    }
+}
+
+
+for(int x = 10; x < 100; x++)
+{
+    int number = x;
+    Console.Write(x + " = ");
+    SimpleSNumbers(number);
+    Console.WriteLine();
+}
